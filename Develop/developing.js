4335ab7d72c5ffba1b294251aac0489e1449f995
XiaoHngm <xiaohngm@foxmail.com>
2019-10-29 02:28:38 +0800

    2019-10-29 02:30

--- Develop/developing.js
+++ Develop/developing.js
@@ -4,21 +4,21 @@ function documentReadyFunction () {
     "use strict";
 
 
-    var screenWidth = screen.availWidth, screenHeight = screen.availHeight;
-    alert(screenWidth);
-    if (screenWidth > 640) { // 电脑
-        $("body").css({"width":screenWidth+"px", "height":screenHeight+"px"});
-    } else { // 手机
-        $("body").css({"width":screenWidth+"px", "height":screenHeight+"px"});
-    }
+    //var screenWidth = screen.availWidth, screenHeight = screen.availHeight;
+    //if (screenWidth > 640) { // 电脑
+        //$("body").css({"width":screenWidth+"px", "height":screenHeight+"px"});
+    //} else { // 手机
+        //$("body").css({"width":screenWidth+"px", "height":screenHeight+"px"});
+    //}
 
+    //$("a").attr("href", "../Games/Whac_A_Mole/game.html");
 
-    $(".score").click(function(e){
+    //$(".score").click(function(e){
         //text = $("#log").text() + "ssss";
         //$("#log").text($(e.target.id+"+p").text());
-        $("#log").text($("#"+e.target.id+"+p").text());
+        //$("#log").text($("#"+e.target.id+"+p").text());
         //topPos = String(Number($("#score").css("top").slice(0,-2))+63)+"px";
         //$("#score").css("top", topPos);
         //$(".score").css("opacity", "0");
-    });
+    //});
 }
--- Home/home.js
+++ Home/home.js
@@ -1,1 +1,13 @@
 
+
+function documentReadyFunction () {
+    "use strict";
+
+
+    var screenWidth = screen.availWidth;
+    if (screenWidth > 640) { // 电脑
+        $("#Game_1 a").attr("href", "Games/Whac_A_Mole/game.html");
+    } else { // 手机
+        $("#Game_1 a").attr("href", "Games/Mobile/Whac_A_Mole/game.html");
+    }
+}
