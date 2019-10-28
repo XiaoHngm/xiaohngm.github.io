bca561f54944f8327a09e3d87a1be42740c00bc4
XiaoHngm <xiaohngm@foxmail.com>
2019-10-28 19:34:34 +0800

    2019-10-28 19:34

--- Develop/developing.js
+++ Develop/developing.js
@@ -1,6 +1,13 @@
 
 
 function documentReadyFunction () {
+    var screenWidth = screen.availWidth, screenHeight = screen.availHeight;
+    if (screenWidth < 400) { // 电脑
+        $("body").css({"width":screenWidth+"px", "height":screenHeight+"px"});
+    } else { // 手机
+        $("body").css({"width":screenWidth+"px", "height":screenHeight+"px"});
+    }
+
     $(".score").click(function(e){
         //text = $("#log").text() + "ssss";
         //$("#log").text($(e.target.id+"+p").text());
--- Games/Whac_A_Mole/game.js
+++ Games/Whac_A_Mole/game.js
@@ -2,6 +2,16 @@
 
 function documentReadyFunction () {
     "use strict";
+
+
+    var screenWidth = screen.availWidth, screenHeight = screen.availHeight;
+    if (screenWidth < 400) { // 电脑
+        $("body").css({"width":screenWidth+"px", "height":screenHeight+"px"});
+    } else { // 手机
+        $("body").css({"width":screenWidth+"px", "height":screenHeight+"px"});
+    }
+
+
     $(".enemy .enemy_img").click(function(e){
         var topPos = String(Number($("#score").css("top").slice(0,-2))+63)+"px";
         $("#score").css("top", topPos);
